d31028240f45f7b5a318cca1fac3662635af3d4b
RadarianDev <radarjameremail@gmail.com>
2025-08-15 11:00:51 +0200

    the actual init

---
+++ data.js
@@ -0,0 +1,106 @@
+const data = {
+    loginBanner: ">>> SCO-AE SECURE LOGIN <<<",
+    users: {
+        "halvorune": {
+            "password": "avNdJhAC",
+            "intro": [
+                "Accessing node: SCO-AE-INT-01",
+                "Connection Secure (AES-512)",
+                "Handshake complete. Session initialized.",
+                "User status: VERIFIED",
+                " ",
+                "> Welcome, Erik Halvorsen.",
+                "> This is a secure node of the Special Coordination Office for Anomalous Events.",
+                "> This system is classified under Council Directive 2013/488/EU.",
+                "> Unauthorized access constitutes a breach of EU cybersecurity and defense law.",
+                " ",
+                "Run the following to initiate probe:",
+                "> validate_user --token=<REDACTED>",
+                " ",
+                "Project JANUS / AE Monitoring / Incident Logs / Memetic Safety Protocols"
+            ],
+        },
+        "radardev": {
+            "password": "axWoW0J0",
+            "intro": [
+                "Accessing node: SCO-AE-INT-01",
+                "Connection Secure (AES-512)",
+                "Handshake complete. Session initialized.",
+                "User status: VERIFIED",
+                " ",
+                "> Welcome, Radim Kozar.",
+                "> This is a secure node of the Special Coordination Office for Anomalous Events.",
+                "> This system is classified under Council Directive 2013/488/EU.",
+                "> Unauthorized access constitutes a breach of EU cybersecurity and defense law.",
+                " ",
+                "Run the following to initiate probe:",
+                "> validate_user --token=<REDACTED>",
+                " ",
+                "Project JANUS / AE Monitoring / Incident Logs / Memetic Safety Protocols"
+            ],
+        },
+        "kingpin": {
+            "password": "3wDfLpaX",
+            "intro": [
+                "Accessing node: SCO-AE-INT-01",
+                "Connection Secure (AES-512)",
+                "Handshake complete. Session initialized.",
+                "User status: VERIFIED",
+                " ",
+                "> Welcome, Jean-Michel Lemoine.",
+                "> This is a secure node of the Special Coordination Office for Anomalous Events.",
+                "> This system is classified under Council Directive 2013/488/EU.",
+                "> Unauthorized access constitutes a breach of EU cybersecurity and defense law.",
+                " ",
+                "Run the following to initiate probe:",
+                "> validate_user --token=<REDACTED>",
+                " ",
+                "Project JANUS / AE Monitoring / Incident Logs / Memetic Safety Protocols"
+            ],
+        },
+        "solvanix": {
+            "password": "Edzn1Y1H",
+            "intro": [
+                "Accessing node: SCO-AE-INT-01",
+                "Connection Secure (AES-512)",
+                "Handshake complete. Session initialized.",
+                "User status: VERIFIED",
+                " ",
+                "> Welcome, Carlos Navarro.",
+                "> This is a secure node of the Special Coordination Office for Anomalous Events.",
+                "> This system is classified under Council Directive 2013/488/EU.",
+                "> Unauthorized access constitutes a breach of EU cybersecurity and defense law.",
+                " ",
+                "Run the following to initiate probe:",
+                "> validate_user --token=<REDACTED>",
+                " ",
+                "Project JANUS / AE Monitoring / Incident Logs / Memetic Safety Protocols"
+            ],
+        },
+        "rozdaci": {
+            "password": "h3t5GBFW",
+            "intro": [
+                "Accessing node: SCO-AE-INT-01",
+                "Connection Secure (AES-512)",
+                "Handshake complete. Session initialized.",
+                "User status: VERIFIED",
+                " ",
+                "> Welcome, Mihaela Vasilescu.",
+                "> This is a secure node of the Special Coordination Office for Anomalous Events.",
+                "> This system is classified under Council Directive 2013/488/EU.",
+                "> Unauthorized access constitutes a breach of EU cybersecurity and defense law.",
+                " ",
+                "Run the following to initiate probe:",
+                "> validate_user --token=<REDACTED>",
+                " ",
+                "Project JANUS / AE Monitoring / Incident Logs / Memetic Safety Protocols"
+            ],
+        },
+    },
+    commands: {
+        "validate_user": "[SYSTEM]: Validation failed. Clearance mismatch.",
+        "probe --logs ae-18": "[PROBE]: Incident AE-18-2025 identified. Trento sector. Classification: MEM-H.",
+        "access incident_log": "[ACCESS DENIED]: Classified above current clearance.",
+        "whoami": "[RESPONSE]: Logged user profile linked to current session."
+    }
+}
